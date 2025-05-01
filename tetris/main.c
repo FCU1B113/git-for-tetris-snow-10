93fb42ca78925052d9bb22a14d119cdb4a41e621
snow-10 <frozenmeteor.3456@gmail.com>
2025-05-01 11:31:52 +0800

    second

--- tetris/main.c
+++ tetris/main.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+#define CANVAS_WIDTH 10
+#define CANVAS_HEIGHT 20
+
 
 typedef enum
 {
@@ -10,7 +15,7 @@ typedef enum
 	S,
 	T,
 	Z
-}Shapeld;
+}ShapeId;
 
 typedef enum
 {
@@ -26,11 +31,27 @@ typedef enum
 
 typedef struct
 {
-	Shapeld shape;
+	ShapeId shape;
 	Color color;
 	int size;
 	char rotates[4][4][4];
 }Shape;
+
+typedef struct {
+	Color color;
+	ShapeId shape;
+	bool current;
+}Block;
+
+typedef struct
+{
+	int x;
+	int y;
+	int score;
+	int rotate;
+	int falltime;
+	ShapeId queue[4];
+}State;
 
 Shape shape[7] = {
 	{
@@ -66,6 +87,34 @@ Shape shape[7] = {
 		}
 	},
 	{
+		.shape = J,
+		.color = BLUE,
+		.size = 3,
+		.rotates =
+		{
+			{
+				{1,0,0},
+				{1,1,1},
+				{0,0,0},
+			},
+			{
+				{0,1,1},
+				{0,1,0},
+				{0,1,0},
+			},
+			{
+				{0,0,0},
+				{1,1,1},
+				{0,0,1},
+			},
+			{
+				{0,1,0},
+				{0,1,0},
+				{1,1,0},
+			},
+		}
+	},
+	{
 		.shape = O,
 		.color = WHITE,
 		.size = 2,
@@ -96,28 +145,24 @@ Shape shape[7] = {
 		.rotates =
 		{
 			{
-				{0,0,0,0},
-				{1,1,1,1},
-				{0,0,0,0},
-				{0,0,0,0},
+				{0,1,1},
+				{1,1,0},
+				{0,0,0},
 			},
 			{
-				{0,0,1,0},
-				{0,0,1,0},
-				{0,0,1,0},
-				{0,0,1,0},
+				{0,1,0},
+				{0,1,1},
+				{0,0,1},
 			},
 			{
-				{0,0,0,0},
-				{0,0,0,0},
-				{1,1,1,1},
-				{0,0,0,0},
+				{0,0,0},
+				{0,1,1},
+				{1,1,0},
 			},
 			{
-				{0,1,0,0},
-				{0,1,0,0},
-				{0,1,0,0},
-				{0,1,0,0},
+				{1,0,0},
+				{1,1,0},
+				{0,1,0},
 			},
 		}
 	},
@@ -125,39 +170,140 @@ Shape shape[7] = {
 		.shape = L,
 		.color = YELLOW,
 		.size = 3,
+		.rotates =
+		{
+			{
+				{0,0,1},
+				{1,1,1},
+				{0,0,0},
+			},
+			{
+				{0,1,0},
+				{0,1,0},
+				{0,1,1},
+			},
+			{
+				{0,0,0},
+				{1,1,1},
+				{1,0,0},
+			},
+			{
+				{1,1,0},
+				{0,1,0},
+				{0,1,0},
+			}
+		}
 	},
 	{
 		.shape = T,
 		.color = PURPLE,
 		.size = 3,
+		.rotates =
+		{
+			{
+				{0,1,0},
+				{1,1,1},
+				{0,0,0},
+			},
+			{
+				{0,1,0},
+				{0,1,1},
+				{0,1,0},
+			},
+			{
+				{0,0,0},
+				{1,1,1},
+				{0,1,0},
+			},
+			{
+				{0,1,0},
+				{1,1,0},
+				{0,1,0},
+			}
+		}
 	},
 	{
 		.shape = Z,
 		.color = RED,
 		.size = 3,
+		.rotates =
+		{
+			{
+				{1,1,0},
+				{0,1,1},
+				{0,0,0},
+			},
+			{
+				{0,0,1},
+				{0,1,1},
+				{0,1,0},
+			},
+			{
+				{0,0,0},
+				{1,1,0},
+				{0,1,1},
+			},
+			{
+				{0,1,0},
+				{1,1,0},
+				{1,0,0},
+			}
+		}
 	},
 };
 
+void setBlock(Block* block, Color color, ShapeId shape, bool current) {
+	block->color = color;
+	block->shape = shape;
+	block->current = current;
+}
+
+void resetBlock(Block* block)
+{
+	block->color = BLACK;
+	block->shape = EMPTY;
+	block->current = false;
+}
+
 int main() {
-	Color cur;
-	//有幾種方塊
-	for (int i = 0; i < 7; i++) {
-		//印出方塊樣式
-		for (int r = 0; r < 4; r++) {
-			for (int s = 0; s < shape[i].size; s++) {
-				for (int t = 0; t < shape[i].size; t++) {
-					if (shape[i].rotates[r][s][t] == 0) {
-						cur = WHITE;
-					}
-					else {
-						cur = shape[i].color;
-					}
-					printf("\033[%dm \033[0m", cur);
-				}
-				printf("\n");
-			}
-			printf("\n");
+	srand(time(NULL));
+
+	State state = {
+		.x = CANVAS_WIDTH / 2,
+		.y = 0,
+		.score = 0,
+		.rotate = 0,
+		.falltime = 0,
+	};
+
+	Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
+
+	for (int i = 0; j < CANVAS_HEIGHT; i++) {
+		for (int j = 0; j < CANVAS_WIDTH; j++) {
+			resetBlock(&canvas[i][j]);
 		}
 	}
+
+	Shape shapeData = shape[1];
+
+	for (int i = 0; i < shapeData.size; i++) {
+		for (int j = 0; j < shapeData.size; j++) {
+			if (shapeData.rotates[state.rotate][i][j] == 1) {
+				setBlock(&canvas[state.y + i][state.x + j], shapeData.color, shapeData.shape, true);
+			}
+		}
+	}
+
+	printf("\033[0;0H\n");
+	for (int i = 0; i < CANVAS_HEIGHT; i++) {
+		printf("|");
+		for (int j = 0; j < CANVAS_WIDTH; j++) {
+			printf("\033[%dm\u3000", canvas[i][j].color);
+		}
+		printf("\033[0m");
+		printf("|\n");
+	}
+
+
 	return 0;
 }
